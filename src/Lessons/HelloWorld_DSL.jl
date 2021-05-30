ccf0af43294d31ca9961acbdaefe3ff3ec6cb4f8
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-05-30 22:27:37 +0530

    Lessons: HelloWorld_DSL: fix typos and split output better into lines
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/Lessons/HelloWorld_DSL.jl
+++ src/Lessons/HelloWorld_DSL.jl
@@ -1,14 +1,14 @@
 #This file contains an Example Lesson plan for Julia tutor.
 
 prompt_1 = ""*
-"Welcome to your first day of class. Today we'll be doing a classic excercise. We will be learning "*
-"how to print text to our console. Along the way we'll cover some ways to handle strings in Julia."*
+"Welcome to your first day of class. Today we'll be doing a classic exercise.\n"*
+"We will be learning how to print text to our console. Along the way we'll cover some ways to handle strings in Julia."*
 "\n"*
-"Strings are defined by double quotes \" \" in julia. For example, we could define a variable, "*
-"called console_output to contain the text \"hello world\" like the following,\n"*
-"console_output = \"hello world\"\n"*
+"Strings are defined by double quotes \" \" in julia.\n\n"*
+"For example, we could define a variable called console_output to contain the text \"hello world\" like the following - \n"*
+"\tconsole_output = \"hello world\"\n"*
 "\n"*
-"Okay now it's your turn,\n"
+"Okay, now it's your turn!\n"
 request_1 = "Define a string titled \'console_output\' to contain the text \"hello world\""
 evaluator_1 = ExpressionEvaluator(:console_output, "hello world")
 hint_1 = """Please ensure your solution is cases sensitive.
@@ -18,8 +18,8 @@ IE: "HeLlO WoRlD" is not the same as "hello world". """
 lesson_1 = Lesson( prompt_1, request_1, evaluator_1, hint_1 )
 
 prompt_2 = ""*
-"Congratulations you have instantiated your first variable in Julia! How might we display this"*
-"text to console? There are two functions that let you do this: print() and println(). Print " *
+"Congratulations you have instantiated your first variable in Julia!\nHow might we display this "*
+"text to console?\nThere are two functions that let you do this:\n\t print() and println().\nPrint " *
 "will display the String or variable as it is, but println will add a new line/carriage return"
 " to the end of it.\n"*
 "\n"*
